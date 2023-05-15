a1a464dfaac8726bf0ef481782e9e67792863fc6
ComputingSquid <3466500-ComputingSquid@users.noreply.replit.com>
2023-05-15 20:29:56 +0000

    added more terms

--- grid.js
+++ grid.js
@@ -1,97 +1,163 @@
 import words from './words.json' assert {type: 'json'};
-let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 let loaded = false;
 
 function shuffle(o) {
-    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 };
 
-function loadvalues() {
+//Gets called on load and whenever you create a new grid
+function loadvalues(seed=null) {
     let condition = false;
-    let commonletters;
+    let commonletters = [];
     let subjects;
-    while (!condition) {
+    document.getElementById("score").className = "hidden";
+
+    
+    //Pick subjects
+    while (commonletters.length < 5) {
         subjects = shuffle(Object.keys(words)).slice(0, 5).sort();
-        let firstletters = {};
+        let firstletters = [];
         for (var i in subjects) {
-            firstletters[subjects[i]] = [];
-            for (var j in words[subjects[i]]) {
-                firstletters[subjects[i]].push(words[subjects[i]][j][0]);
-            }
+            firstletters.push(Object.keys(words[subjects[i]]))
         }
-        commonletters = _.intersection.apply(_, Object.values(firstletters));
-        condition = commonletters.length >= 5
+        commonletters = _.intersection.apply(_, firstletters);
+        //if (!commonletters.includes("X")) {commonletters = [];}
     }
 
-    //console.log(subjects)
+    //Pick letters
     commonletters = [...new Set(commonletters)];
     commonletters = shuffle(commonletters).slice(0, 5).sort();
-    
+
+    //Create grid
     let text = "";
-    
     for (let i = 0; i < 5; i++) {
-        document.getElementById("ltr"+String(i+1)).innerHTML = commonletters[i];
+        document.getElementById("ltr" + String(i + 1)).innerHTML = commonletters[i];
         for (let j = 0; j < 5; j++) {
             const td = document.createElement("td");
             const inp = document.createElement("input");
             inp.removeAttribute('class')
-            inp.setAttribute("id", String(i)+String(j));
+            inp.setAttribute("id", String(i) + String(j));
             inp.setAttribute("autocomplete", "off");
             td.appendChild(inp);
-            const element = document.getElementById("row" + String(i+1));
+            const element = document.getElementById("row" + String(i + 1));
             element.appendChild(td);
         }
     }
     for (let i = 0; i < 5; i++) {
-        document.getElementById("cat"+String(i+1)).innerHTML = subjects[i];
+        document.getElementById("cat" + String(i + 1)).innerHTML = subjects[i];
     }
 
+    /*Copy a seed because reasons
+    copybutton = document.getElementById("copyseed")
+    copybutton.addEventListener("click", () => {
+        alert("hi")
+        let tocopy = "";
+        let zfillto = String(Object.keys(words).length).length
+        for (let i = 0; i < 5; i++) {
+            tocopy += String(Object.keys(words).indexOf(subjects[i]))
+        }
+        for (let i = 0; i < 5; i++) {
+            tocopy += commonletters[i]
+        }
+        document.getElementById("seed").value = tocopy
+        navigator.clipboard.writeText(tocopy);
+    });*/
+
+    //Mark answers as correct or incorrect
     function score() {
         let correctwords;
         let wordstomatch;
-        
+        let correctlist = [];
+
         for (let i = 0; i < 5; i++) {
+            correctlist[i] = [];
             for (let j = 0; j < 5; j++) {
-                correctwords = words[subjects[j]].filter(word => word[0] == commonletters[i]);
-                
+                correctwords = words[subjects[j]][commonletters[i]];
+
                 wordstomatch = correctwords.map(word => word.toLowerCase());
-                let input = document.getElementById(String(i)+String(j));
-                console.log(input.value)
-                //input.setAttribute("value", correctwords[0]);
+                let input = document.getElementById(String(i) + String(j));
+                input.setAttribute("readonly", "");
                 if (wordstomatch.includes(input.value.toLowerCase())) {
-                    input.className = "correct"
+                    input.className = "correct";
+                    correctlist[i].push(true);
                 } else {
-                    input.value = correctwords.join(", ");
                     input.className = "incorrect";
+                    correctlist[i].push(false);
                 }
-                
+
             }
         }
+        return correctlist
     }
 
+    //Submit grid
     let submit = document.getElementById("submit");
-    function submitsheet() {
-        score()
+    submit.addEventListener("click", () => {
+        //get score
+        let correctlist = score();
+        let points = 0;
+        for (let i = 0; i < correctlist.length; i++) {
+            points += correctlist[i].filter(x => x).length ** 2;
+        }
+        correctlist = _.unzip(correctlist);
+        for (let i = 0; i < correctlist[0].length; i++) {
+            points += correctlist[i].filter(x => x).length ** 2;
+        }
+        let scoredisp = document.getElementById("score");
+        scoredisp.className = "";
+        scoredisp.innerHTML = points + " points";
         submit = document.getElementById("submit");
         submit.className = "hidden";
+
+        //Toggle between correct answers and your answers
+        let oldbutton = document.getElementById("showbutton");
+        let showbutton = oldbutton.cloneNode(true);
+        oldbutton.parentNode.replaceChild(showbutton, oldbutton);
+        showbutton.value = "Show Correct Answers";
+        showbutton.className = "";
+        let correctwords;
+        function showanswers() {
+            let inputs = [];
+            for (let i = 0; i < 5; i++) {
+                inputs.push([])
+                for (let j = 0; j < 5; j++) {
+                    correctwords = words[subjects[j]][commonletters[i]];
+                    let input = document.getElementById(String(i) + String(j));
+                    inputs[i].push(input.value);
+                    input.value = correctwords.join(", ");
+                }
+            }
+            showbutton.value = "Show Your Answers";
+            function showinputs() {
+                for (let i = 0; i < 5; i++) {
+                    for (let j = 0; j < 5; j++) {
+                        let input = document.getElementById(String(i) + String(j));
+                        input.value = inputs[i][j];
+                    }
+                }
+                showbutton.value = "Show Correct Answers";
+                showbutton.addEventListener("click", showanswers, { once: true });
+            }
+            showbutton.addEventListener("click", showinputs, { once: true });
+        }
+        showbutton.addEventListener("click", showanswers, { once: true });
+
+        //Create new grid when the user is done
         let nextbutton = document.getElementById("nextbutton");
-        nextbutton.className = "shown";
-        submit.removeEventListener("click", submitsheet);
+        nextbutton.className = "";
         function next() {
+            let showbutton = document.getElementById("showbutton");
+            showbutton.className = "hidden";
             let submit = document.getElementById("submit");
-            submit.className = "shown";
+            submit.className = "";
             let nextbutton = document.getElementById("nextbutton");
             nextbutton.className = "hidden";
             document.querySelectorAll("td").forEach(word => word.remove());
             loadvalues();
         }
-        nextbutton.addEventListener("click", next);
-    }
-    submit.addEventListener("click", submitsheet);
-    loaded = true;
-    
+        nextbutton.addEventListener("click", next, { once: true });
+    }, { once: true });
 }
 
 window.addEventListener('load', loadvalues)
-    
---
+++ study.js
@@ -0,0 +1,217 @@
+import words from 'https://replit.com/@EmojiGame/LimegreenSaneDehardwarization#words.json' assert {type: 'json'};
+let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+let allsubjects = Object.keys(words);
+let corrects;
+
+//Called on load and when you press "Go Back" (Creates grid of subject links you press to study)
+function loadTable() {
+    let grid = document.getElementById("grid");
+    for (let i = 0; i < allsubjects.length; i++) {
+        let td = document.createElement("td");
+        let link = document.createElement("a");
+        link.setAttribute("id", i);
+        link.setAttribute("href", "#");
+        link.setAttribute("onclick", "study(this.id)");
+        link.appendChild(document.createTextNode(allsubjects[i]));
+        td.appendChild(link);
+        grid.appendChild(td);
+    }
+}
+window.addEventListener('load', loadTable);
+
+//Code stolen and modified from stackoverflow
+var Stopwatch = function(elem, options) {
+    var timer = elem, offset, clock, interval;
+
+    // default options
+    options = options || {};
+    options.delay = options.delay || 1;
+
+    // initialize
+    reset();
+
+    // private functions
+    function createTimer() {
+        return document.createElement("span");
+    }
+
+    function start() {
+        if (!interval) {
+            offset = Date.now();
+            interval = setInterval(update, options.delay);
+        }
+    }
+
+    function stop() {
+        if (interval) {
+            clearInterval(interval);
+            interval = null;
+        }
+    }
+
+    function reset() {
+        clock = 0;
+        render(0);
+    }
+
+    function update() {
+        clock += delta();
+        render();
+    }
+
+    function render() {
+        let seconds = Math.round(clock) / 1000;
+        let minutes = Math.floor(seconds/60)
+        seconds %= 60;
+        timer.innerHTML = String(minutes) + ":" + String(seconds.toFixed(3).padStart(6, "0"));
+    }
+
+    function delta() {
+        var now = Date.now(),
+            d = now - offset;
+
+        offset = now;
+        return d;
+    }
+
+    // public API
+    this.start = start;
+    this.stop = stop;
+    this.reset = reset;
+};
+
+var thyme;
+
+//Gets called when you press a link
+function study(id) {
+    //Hide stuff
+    let input = document.getElementById("input");
+    input.value = "";
+    input.removeAttribute("readonly");
+    document.getElementById("table").innerHTML = "";
+    document.getElementById("giveup").className = "";
+    document.getElementById("restart").className = "hidden";
+    document.getElementById("subject").innerHTML = allsubjects[id];
+    document.getElementById("grid").className = "hidden";
+    document.getElementById("rand").className = "hidden";
+    document.getElementById("quiz").className = "";
+
+    //Timer stuff
+    thyme = new Stopwatch(document.getElementById("timer"));
+    document.getElementById("input").addEventListener("focus", () => {
+        thyme.start();
+    }, {once: true});
+    //thyme.start()
+
+    //Reset confetti counter
+    corrects = 0;
+
+    //Create the form where your answers show up
+    let table = document.getElementById("table");
+    table.className = "";
+    let correctanswers = words[allsubjects[id]];
+    let letterslist = Object.keys(correctanswers);
+    for (let i = 0; i < letterslist.length; i++) {
+        //Create a bunch of elements for every letter
+        let row = document.createElement("tr");
+        let th = document.createElement("th")
+        th.appendChild(document.createTextNode(letterslist[i]));
+        let td = document.createElement("td");
+        let out = document.createElement("input");
+        out.setAttribute("readonly", "");
+        out.setAttribute("id", letterslist[i]);
+        out.className = "smallfont";
+        td.appendChild(out);
+        row.appendChild(th);
+        row.appendChild(td);
+        table.appendChild(row);
+    }
+}
+window.study = study;
+
+//Gets called when you click "Random Topic"
+function studyrand() {
+    study(Math.floor(Math.random() * allsubjects.length));
+}
+window.studyrand = studyrand
+
+//Gets called when you hit enter after typing a guess
+function guess(value, inputbox) {
+    //Define variables
+    let correctanswers = words[document.getElementById("subject").innerHTML];
+    let letterslist = Object.keys(correctanswers);
+
+    //Iterate through the answers to find one that matches
+    for (let i = 0; i < letterslist.length; i++) {
+        //Account for plurals and missing punctuation
+        let wordstomatch = correctanswers[letterslist[i]].map(word => pluralize.singular(word).toLowerCase().replace(/ |-|'/g, ""));
+        if (wordstomatch.includes(pluralize.singular(value).toLowerCase().replace(/ |-|'/g, ""))) {
+            //Mark as correct or incorrect
+            let answerbox = document.getElementById(letterslist[i]);
+            if (answerbox.className != "smallfont correct") {
+                inputbox.value = "";
+                answerbox.className = "smallfont correct";
+                answerbox.value = correctanswers[letterslist[i]].join(", ");
+                corrects++;
+                if (corrects == letterslist.length) {
+                    //Called when you get everything right. Confetti :)
+                    let input = document.getElementById("input");
+                    input.value = "";
+                    input.setAttribute("readonly", "");
+                    thyme.stop()
+                    document.getElementById("giveup").className = "hidden"
+                    document.getElementById("restart").className = "";
+                    startConfetti()
+                }
+            }
+            break
+        }
+    }
+}
+window.guess = guess;
+
+//Called when you press "Give Up"
+function giveup() {
+    //Mark stuff as incorrect
+    let correctanswers = words[document.getElementById("subject").innerHTML];
+    let letterslist = Object.keys(correctanswers);
+    for (let i = 0; i < letterslist.length; i++) {
+        let answerbox = document.getElementById(letterslist[i]);
+        for (let j = 0; j < correctanswers[letterslist[i]].length; j++) {
+            if (answerbox.className != "smallfont correct") {
+                answerbox.value = correctanswers[letterslist[i]].join(", ");
+                answerbox.className = "smallfont incorrect"
+            }
+        }
+    }
+
+    //Hide stuff
+    //You 
+    thyme.start()
+    thyme.stop()
+    document.getElementById("input").setAttribute("readonly", "");
+    document.getElementById("giveup").className = "hidden"
+    document.getElementById("restart").className = ""
+}
+window.giveup = giveup
+
+//Called when you press "Restart"
+function restart() {
+    stopConfetti()
+    let id = allsubjects.indexOf(document.getElementById("subject").innerHTML)
+    study(id)
+}
+window.restart = restart;
+
+//Called when you press "Go Back"
+function goback() {
+    //Hide stuff
+    thyme.stop()
+    document.getElementById("input").value = "";
+    document.getElementById("grid").className = "";
+    document.getElementById("rand").className = "margintop";
+    document.getElementById("quiz").className = "hidden"
+    document.getElementById("table").innerHTML = "";
+    removeConfetti()
+}
+window.goback = goback
